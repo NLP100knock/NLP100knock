a25d1857821cfc00fc2fe048adc1a11e3d314e25
ihmana <b1013067@fun.ac.jp>
2016-07-16 23:34:49 +0900

    add

--- ch02_11.py
+++ ch02_11.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
+'''
+タブ1文字につきスペース1文字に置換せよ．
+確認にはsedコマンド，trコマンド，もしくはexpandコマンドを用いよ．
+'''
 
-datafile = open('hightemp.txt')
+
+datafile = open('./data/hightemp.txt')
 
 for line in datafile:
     print line.replace('\t', ' ')
+
